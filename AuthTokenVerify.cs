a08385d285209cfd8c0dd95f44ab62c4752957c4
Shalaka03 <36564137+Shalaka03@users.noreply.github.com>
2018-03-22 22:38:58 +0530

    Add files via upload

---
+++ AuthTokenVerify.cs
@@ -0,0 +1,30 @@
+﻿using student.webapi.core.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Samplewebapi.Filter
+{
+    public class AuthTokenVerify
+    {
+        private ILoginRepository _loginRepository;
+        public AuthTokenVerify(ILoginRepository loginRepository)
+        {
+            _loginRepository = loginRepository;
+        }
+
+        public dynamic VerifyAuthToken(string authHeader)
+        {
+            bool isValid = false;
+
+            if (authHeader != null)
+            {
+                var response = _loginRepository.ValidateToken(authHeader);
+                return response;
+
+            }
+            return isValid;
+        }
+    }
+}
---
+++ AuthorizeTokenFilter.cs
@@ -0,0 +1,39 @@
+﻿using student.webapi.core.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace Samplewebapi.Filter
+{
+    public class AuthorizeTokenFilter : AuthorizationFilterAttribute
+    {
+
+        private const string AUTHORIZATION = "Authorization";
+        AuthTokenVerify authTokenVerify = null;
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            if (actionContext.Request.Headers.Authorization == null)
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+            }
+            else
+            {
+                var authHeader = actionContext.Request.Headers.Authorization.Parameter;
+                //Get the instance of the repository using the dependancy resolver 
+                var _loginRepository = (ILoginRepository)actionContext.RequestContext.Configuration.DependencyResolver.GetService(typeof(ILoginRepository));
+
+                authTokenVerify = new AuthTokenVerify(_loginRepository);
+                if (!authTokenVerify.VerifyAuthToken(authHeader))
+                {
+                    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized, "Invalid token");
+                }
+            }
+            base.OnAuthorization(actionContext);
+        }
+    }
+}
